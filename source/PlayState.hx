30dfeb978aaf7fd664421a1269bfde14d120e47d
IdealistCat <sinconsistencia@gmail.com>
2025-01-26 14:39:30 -0600

    create player

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,11 +1,17 @@
 package;
 
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
+	var player:FlxSprite = new FlxSprite(0, 0).makeGraphic(32, 32, FlxColor.LIME);
+
 	override public function create()
 	{
+		add(player);
+
 		super.create();
 	}
 
