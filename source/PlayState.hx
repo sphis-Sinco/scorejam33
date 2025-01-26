eef5d020c801107438130da76e83983a2cbf3169
IdealistCat <sinconsistencia@gmail.com>
2025-01-26 14:44:27 -0600

    make a bullet grp and weapon charge stufs

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2,14 +2,21 @@ package;
 
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.group.FlxGroup.FlxTypedGroup;
 import flixel.util.FlxColor;
 
 class PlayState extends FlxState
 {
 	var player:FlxSprite = new FlxSprite(0, 0).makeGraphic(32, 32, FlxColor.LIME);
 
+	var weapon_charge:Int = 0;
+	var max_weapon_charge:Int = 10;
+
+	var bullet_group:FlxTypedGroup<FlxSprite> = new FlxTypedGroup<FlxSprite>();
+
 	override public function create()
 	{
+		add(bullet_group);
 		add(player);
 
 		super.create();
