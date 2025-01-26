18d23f6c9ee3a067dea20ec906c8301c664b2812
IdealistCat <sinconsistencia@gmail.com>
2025-01-26 15:37:04 -0600

    a web fix thing

--- source/Main.hx
+++ source/Main.hx
@@ -10,6 +10,8 @@ class Main extends Sprite
 	public function new()
 	{
 		FlxG.save.bind('scorejam33', Application.current.meta.get('company'));
+		FlxG.save.data.hiscore ??= 0;
+		
 		super();
 		addChild(new FlxGame(0, 0, PlayState));
 	}
