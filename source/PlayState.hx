45ccc8564fda0dafcd89264ba968ef2250f5a158
IdealistCat <sinconsistencia@gmail.com>
2025-01-26 15:00:36 -0600

    player vertical movement

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -10,6 +10,7 @@ import flixel.util.FlxColor;
 class PlayState extends FlxState
 {
 	var player:FlxSprite = new FlxSprite(0, 0).makeGraphic(32, 32, FlxColor.LIME);
+	var player_offscreen_padding:Float = 16;
 
 	var weapon_charge:Int = 0;
 	var max_weapon_charge:Int = 10;
@@ -35,10 +36,29 @@ class PlayState extends FlxState
 	var key_space:Bool;
 	var key_z:Bool;
 
+	var key_up:Bool;
+	var key_down:Bool;
+
 	override public function update(elapsed:Float)
 	{
 		key_space = FlxG.keys.justReleased.SPACE;
 		key_z = FlxG.keys.justReleased.Z;
+
+		key_up = FlxG.keys.pressed.UP;
+		key_down = FlxG.keys.pressed.DOWN;
+
+		if (key_up)
+		{
+			player.y -= 10;
+			if (player.y < 0 + player_offscreen_padding)
+				player.y = 0 + player_offscreen_padding;
+		}
+		if (key_down)
+		{
+			player.y += 10;
+			if (player.y > FlxG.height - player.height - player_offscreen_padding)
+				player.y = FlxG.height - player.height - player_offscreen_padding;
+		}
 
 		if (key_z)
 		{
