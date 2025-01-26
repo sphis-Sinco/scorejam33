5adac3f605b9a906ee6431a7cfa4482957f6fbc0
IdealistCat <sinconsistencia@gmail.com>
2025-01-26 14:47:48 -0600

    weapon charge text

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,8 +1,10 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
 import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.text.FlxText;
 import flixel.util.FlxColor;
 
 class PlayState extends FlxState
@@ -12,6 +14,8 @@ class PlayState extends FlxState
 	var weapon_charge:Int = 0;
 	var max_weapon_charge:Int = 10;
 
+	var weaponChargeText:FlxText = new FlxText(0, 0, 0, "CHARGE: 69", 16);
+
 	var bullet_group:FlxTypedGroup<FlxSprite> = new FlxTypedGroup<FlxSprite>();
 
 	override public function create()
@@ -19,11 +23,26 @@ class PlayState extends FlxState
 		add(bullet_group);
 		add(player);
 
+		weaponChargeText.setPosition(8, 8);
+		add(weaponChargeText);
+
 		super.create();
 	}
+	var key_space:Bool;
 
 	override public function update(elapsed:Float)
 	{
+		key_space = FlxG.keys.justReleased.SPACE;
+
+		if (key_space)
+		{
+			weapon_charge++;
+			if (weapon_charge > max_weapon_charge)
+				weapon_charge = max_weapon_charge;
+		}
+
+		weaponChargeText.text = "CHARGE: " + weapon_charge;
+
 		super.update(elapsed);
 	}
 }
