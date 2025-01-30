de4811878be22b68d3d8c11f341a91248d16fa31
IdealistCat <sinconsistencia@gmail.com>
2025-01-29 21:13:17 -0600

    say the value given, will help with debugging and help with debugging even more by just tracing the errors

--- source/Save.hx
+++ source/Save.hx
@@ -6,8 +6,10 @@ class Save
 {
 	public static var HIGHSCORE:Int = 0;
 
-	public static function updateValue(value:String, ?newvalue:Dynamic)
+	public static function updateValue(value:String, ?newvalue:Dynamic, ?pos:haxe.PosInfos)
 	{
+		var errors:Array<String> = [];
+
 		updateFlxGSaves();
 		
 		switch (value.toLowerCase())
@@ -20,9 +22,19 @@ class Save
 				catch (e)
 				{
 					HIGHSCORE = (FlxG.save.data.hiscore > HIGHSCORE) ? FlxG.save.data.hiscore : 0;
-					throw 'newvalue should be Int for field: ${value.toLowerCase()}';
+					errors.push('newvalue should be Int for highscore field. Given value: "${newvalue}"');
 				}
 		}
+		if (errors.length != 0)
+		{
+			trace('${errors.length} error(s) found while updating save values:');
+
+			for (err in errors)
+			{
+				trace('- $err');
+			}
+		}
+
 		updateFlxGSaves();
 	}
 
